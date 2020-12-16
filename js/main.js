163f4200c88140e7549f34e534bb267f15f6ad9c
yukinashi <yukinashi0311@gmail.com>
2020-12-16 23:31:32 +0900

    ボタン実装

--- js/main.js
+++ js/main.js
@@ -7,9 +7,14 @@ let main = function() {
         let element = $(article).find('.css-901oao.r-18jsvk2.r-1tl8opc.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0');
         if (element.attr("loaded") !== "true") {
             element.attr("loaded", "true");
-            if (element[0].innerText !== undefined) console.log(element[0].innerText);
+            if (element[0] !== undefined) console.log(element[0].innerText);
             element.parents(".css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1mi0q7o").find(".css-1dbjc4n.r-k4xj1c.r-18u37iz.r-1wtj0ep > .css-1dbjc4n.r-1d09ksm.r-18u37iz.r-1wbh5a2").append('<div dir="auto" aria-hidden="true" class="css-901oao r-m0bqgq r-1q142lx r-1tl8opc r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-ou255f r-qvutc0"><span class="css-901oao css-16my406 r-1tl8opc r-ad9z0x r-bcqeeo r-qvutc0">·</span></div>' +
-                '<span class="get_achievement_btn">実績解除</span>');
+                    '<a><span class="get_achievement_btn">実績解除</span></a>')
+                .find("span.get_achievement_btn")
+                .on("click", function() {
+                    let element = $(this).parents(".css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1mi0q7o").find('.css-901oao.r-18jsvk2.r-1tl8opc.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0');
+                    if (element[0] !== undefined) console.log(element[0].innerText);
+                })
         }
     });
 }
