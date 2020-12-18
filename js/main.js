b73c803a278fea50acee905e1e5b82ecc6d597da
yukinashi <yukinashi0311@gmail.com>
2020-12-18 17:46:56 +0900

    最適化と保存

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-let main = function() {
-    setTimeout(main, 500);
-    if (location.href !== "https://twitter.com/home") {
-        return;
-    }
-    $('article.css-1dbjc4n.r-1loqt21.r-18u37iz.r-1ny4l3l.r-1udh08x.r-1yt7n81.r-ry3cjt.r-o7ynqc.r-6416eg').each(function(eq, article) {
-        let element = $(article).find('.css-901oao.r-18jsvk2.r-1tl8opc.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0');
-        if (element.attr("loaded") !== "true") {
+(function() {
+    // 一意のid:[ 実績の名前, 正規表現, ポイント]
+    let achievement_list = {
+        "a": ["チュートリアル", ".*", 10],
+        "b": ["コロナおじさん", "コロナはただの風邪", 10],
+        "c": ["チーズ牛丼", "(チーズ牛丼|チー牛)", 10]
+    };
+
+    let got_achievement_list = {};
+    let main = function() {
+        setTimeout(main, 500);
+
+        let split = location.href.split('/');
+        if (split[3] !== "home" && split[4] !== "status") return;
+
+        $('article.css-1dbjc4n.r-1loqt21.r-18u37iz.r-1ny4l3l.r-1udh08x.r-1yt7n81.r-ry3cjt.r-o7ynqc.r-6416eg').each(function(eq, article) {
+            let element = $(article).find('.css-901oao.r-18jsvk2.r-1tl8opc.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0');
+            if (element.attr("loaded") === "true") return;
             element.attr("loaded", "true");
             if (element[0] !== undefined)
                 if (search_id(element[0].innerText));
@@ -15,8 +25,11 @@ let main = function() {
                     '<a><span class="get_achievement_btn">実績解除</span></a>')
                 .find("span.get_achievement_btn")
                 .on("click", function() {
-                    let element = $(this).parents(".css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1mi0q7o").find('.css-901oao.r-18jsvk2.r-1tl8opc.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0');
+                    let parents = $(this).parents(".css-1dbjc4n.r-1iusvr4.r-16y2uox.r-1777fci.r-1mi0q7o");
+                    let element = parents.find('.css-901oao.r-18jsvk2.r-1tl8opc.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-bnwqim.r-qvutc0');
+                    let href = parents.find("a.css-4rbku5.css-18t94o4.css-901oao.r-m0bqgq.r-1loqt21.r-1q142lx.r-1tl8opc.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-3s2u2q.r-qvutc0").attr("href");
                     let id = search_id(element[0].innerText);
+                    if (achievement_list[id] === undefined) $(this).remove();
                     $(this).text(achievement_list[id][2] + "exp獲得")
                         .off("click");
                     let text = "実績解除[" + achievement_list[id][0] + "]";
@@ -24,27 +37,25 @@ let main = function() {
                     setTimeout(function() {
                         that.text(text)
                     }, 2000);
+                    got_achievement_list[id] = href;
+                    console.log(got_achievement_list);
                     delete achievement_list[id];
                 })
-        }
-    });
-}
-setTimeout(main, 500);
-// 一意のid:[ 実績の名前, 正規表現, ポイント]
-let achievement_list = {
-    "a": ["チュートリアル", ".*", 10],
-    "b": ["コロナおじさん", "コロナはただの風邪", 10],
-    "c": ["チーズ牛丼", "(チーズ牛丼|チー牛)", 10]
-};
+
+        });
+    }
+    setTimeout(main, 500);
+
+    let search_id = function(text) {
+        let ret = false;
+        $.each(achievement_list, function(id, column) {
+            if (text.match(new RegExp(column[1])) !== null) {
+                ret = id;
+                return false;
+            }
+        });
+        return ret;
+    }
 
 
-let search_id = function(text) {
-    let ret = false;
-    $.each(achievement_list, function(id, column) {
-        if (text.match(new RegExp(column[1])) !== null) {
-            ret = id;
-            return false;
-        }
-    });
-    return ret;
-}
+})()
